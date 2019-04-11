c4918111733ae57a0a53230edb557e4be8e7384d
Natasha Lovato <nmarshlovato@cnm.edu>
2019-04-11 16:47:31 -0600

    Changed pre/post conditions and Interaction flow.

--- epic/usercase.php
+++ epic/usercase.php
@@ -13,7 +13,7 @@
 		<p><strong>Title:</strong> Adding merchandise to a wishlist</p>
 		<p><strong>Description:</strong> Harley wants to add accesories & apparel to her wishlist.</p>
 		<p><strong>Name of User &amp; their role:</strong> Harley, madly in love with 'The Joker' & enjoys terrorizing Gotham City.</p>
-		<p><strong>Usage Preconditions:</strong> Amazon must have access to apparel and accesories.</p>
+		<p><strong>Usage Preconditions:</strong> Harley must have an Amazon account and be logged in.</p>
 		<p><strong>Usage Postconditions:</strong> Merchandise must be able to be added to a wishlist.</p>
 
 
@@ -22,9 +22,9 @@
 		<p><strong>Interaction Flow:</strong></p>
 
 		<ol>
-			<li>Harley enters www.amazon.com into her web browser.</li>
-			<li>Server returns Amazon's main web page.</li>
+
 			<li>Harley selects profile to access "Wishlist" & sees that her wishlist is empty.</li>
+			<li>Harley opens search bar and searches favorite apparel and accesories.</li>
 			<li>Harley adds an accessory along with a few apparel items to wishlist.</li>
 			<li>Harley then sees her selected items in her wishlist.</li>
 			<li>Server loads Harley's wishlist page on her Amazon account.</li>
