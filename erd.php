eda0a99189531064a6f1f3388655e587c92efa2a
Natasha Lovato <nmarshlovato@cnm.edu>
2019-04-12 10:05:08 -0600

    Added erd image

--- erd.php
+++ erd.php
@@ -5,3 +5,14 @@
 		<meta charset="UTF-8">
 		<title>Entity Relationship Diagram</title>
 	</head>
+
+	<body>
+
+		<h1>Entity Relationship Diagram</h1>
+
+		<br>
+		<br>
+
+		<img src="data-design-erd.jpg" width=" 450px" alt="erd">
+
+	</body>
