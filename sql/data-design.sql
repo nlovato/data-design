37db8bd3ce684ec821e14d38a519d8ba55123c17
Natasha Lovato <nmarshlovato@cnm.edu>
2019-04-12 20:20:54 -0600

    Added sql tables to data-design.sql file

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,13 +1,26 @@
-create table user (
-userId binary(16) not null,
-userLogin varchar(32) not null,
-userHAsh char(97) not null,
-userEmail varchar(128) not null,
-userPhone varchar(32),
-primary key(userId)
-);
+	drop table if exists addList;
+	drop table if exists itemId;
+	drop table if exists userId;
 
-create table item (
-itemId binary(16) not null,
+		create table user (
+		userId binary(16) not null,
+		userLogin varchar(32) not null,
+		userHAsh char(97) not null,
+		userEmail varchar(128) not null,
+		userPhone varchar(32),
+		primary key(userId)
+		);
 
-)
+	create table item (
+	itemId binary(16) not null,
+	itemContent binary(140) not null,
+	itemReview (itemContent),
+	index(itemId),
+	foreign key(itemId)
+	);
+
+		create table addlist (
+		addList not null,
+		addWishList,
+		foreign key(addList) references(customerId)
+		);
