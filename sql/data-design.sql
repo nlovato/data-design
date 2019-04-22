454c1f0b8c23f96537bbdcafd6c854b3fcc206c3
Natasha Lovato <49003862+nlovato@users.noreply.github.com>
2019-04-22 10:36:54 -0600

    Update data-design.sql

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,6 +1,6 @@
-	/**
-	these statements will drop the tables and re-add them
-	 */
+/**
+**these statements will drop the tables and re-add them
+*/
 
 drop table if exists itemList;
 drop table if exists list;
@@ -10,7 +10,6 @@ drop table if exists user;
 /**
 user entity
  */
-
 create table user (
 	userId binary(16) not null,
 	userLogin varchar(64) not null,
@@ -18,37 +17,33 @@ create table user (
 	userEmail varchar(128) not null,
 	userPhone varchar(32),
 	primary key(userId)
-	);
+);
 
 /**
 item entity
  */
-
 create table item (
 	itemId binary(16) not null,
 	itemContent varchar(140) not null,
 	index(itemId),
 	primary key(itemId)
-	);
+);
 
 /**
 create the list entity
  */
-
 create table list (
 	listId binary(16) not null,
 	listUserId binary(16) not null,
 	listName varchar(32) not null,
 	foreign key(listUserId) references user(UserId),
 	primary key(listId)
-	);
+);
 
 /**
 itemList entity (weak entity from many to one
 for user --> list
  */
-
-
 create table itemList (
 	itemListListId binary(16) not null,
 	itemListItemId binary(16) not null,
@@ -56,10 +51,11 @@ create table itemList (
 	index(itemListItemId),
 	foreign key(itemListListId) references list(listId),
    foreign key(itemListItemId) references item(itemId),
+
 /**
 primary key = composite foreign key; with two foreign keys
 */
-	primary key(itemListListId, itemListItemId)
+primary key(itemListListId, itemListItemId)
 );
 
 
